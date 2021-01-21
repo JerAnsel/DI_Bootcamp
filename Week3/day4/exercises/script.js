be4187c9f35909236d13ae2a66490c751a0d8d60
Jeremy Ansel <jeremyansel@hotmail.com>
2021-01-21 20:39:11 +0200

    Finished w3d4

---
+++ Week3/day4/exercises/script.js
@@ -0,0 +1,21 @@
+let x = 3;
+let y = 2;
+
+if(x>y){
+    console.log("x is bigger");
+}
+
+else{
+    console.log("y is bigger");
+}
+
+let newDog = "Chihuahua";
+
+console.log("there are ", newDog.length, "characters in newdog.");
+
+console.log(newDog.toUpperCase());
+console.log(newDog.toLowerCase());
+
+if(newDog == "Chihuahua"){
+    console.log("I love chihuahua. It's my favorite dog breed.");
+}
