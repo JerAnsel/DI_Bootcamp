90dd38058102fe997fc9bf509bb4a388d6ccdbcd
Jeremy Ansel <jeremyansel@hotmail.com>
2021-01-26 15:02:49 +0200

    Added w3d2 exercises

---
+++ Week3/day2/ex1/script.js
@@ -0,0 +1,15 @@
+/*Guesses in comments*/
+
+5 + "34" // 39 Correct: "534"
+5 - "4" // 1 Correct: 1
+10 % 5 // 0 Correct: 0
+5 % 10 // 5 Correct: 5
+"Java" + "Script" // "JavaScript" Correct: "JavaScript"
+" " + " " // "  " Correct: "  "
+" " + 0 // " 0" Correct: " 0"
+true + true // true Correct: 2
+true + false // true Correct: 1
+false + true  // true Correct: 1
+false - true  // true Correct: -1
+3 - 4 // -1 Correct: -1
+"Bob" - "bill" // Error Correct: NaN
---
+++ Week3/day2/ex2/script.js
@@ -0,0 +1,5 @@
+let me = ["my", "favorite", "color", "is", "blue"];
+
+let result = me.join(" ");
+
+console.log(result);
